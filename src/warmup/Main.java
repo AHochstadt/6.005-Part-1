c52463ace95994d8ad74f54ae6d6fd1433229d17
Lauren Clark <Catherine@24-1-153.dynamic.csail.mit.edu>
2014-04-11 17:16:40 -0400

    new shit

--- src/warmup/Main.java
+++ src/warmup/Main.java
@@ -9,12 +9,12 @@ public class Main {
      * TODO: describe your main function's command line arguments here
      */
     public static void main(String[] args) {
-        Ball testBall = new Ball(5.0, 5.0, -45.0, 5.0);
+        Ball testBall = new Ball(1.0, 5.0, -45.0, 5.0);
         Ball[] balls = {testBall};
         
         Board testBoard = new Board(20, 20, balls);
         
-        for (int i = 0; i < 200; i++) {
+        for (int i = 0; i < 2000; i++) {
             System.out.println(testBoard.update(0.05));
             try {
                 Thread.sleep(50);
