428115929da1e0c06ffd1950686bf0ba27331e05
Lauren Clark <Catherine@Catherines-MacBook-Air.local>
2014-04-12 18:26:04 -0400

    fixed error

--- src/Bumper.java
+++ src/Bumper.java
@@ -2,13 +2,14 @@
 public class Bumper implements Stationary {
 
     @Override
-    public void getLocationBounds() {
+    public boolean inBounds(Ball b) {
+        return false;
         // TODO Auto-generated method stub
         
     }
-    //for bumper, the ball will move away with an increased velocity
+    //
     @Override
-    public void getEffect() {
+    public void getEffect(Ball b) {
         // TODO Auto-generated method stub
         
     }
