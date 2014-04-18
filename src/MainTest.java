aa131aae575ed1381e3e7c20d807f5774cbcc369
andrew16 <andrew16@mit.edu>
2014-04-18 05:50:21 -0400

    commit

--- src/MainTest.java
+++ src/MainTest.java
@@ -36,10 +36,11 @@ public class MainTest {
         System.out.println(listener.getFlippers());
         System.out.println(listener.getBalls());**/
         
-        Board b = new Board("/Users/Catherine/Dropbox/Classes/Spring2014/6.005/pingball-phase1/src/sampleBoard4.txt");
+        //Board lauren = new Board("/Users/Catherine/Dropbox/Classes/Spring2014/6.005/pingball-phase1/src/sampleBoard4.txt");
+        Board andrew = new Board("C:/Users/ahochstadt/pingball-phase1/src/sampleBoard1.txt");
         while (true) {
-            Thread.sleep(500);
-            System.out.println(b.update(0.05));
+            Thread.sleep(50);
+            System.out.println(andrew.update(0.05));
             
         }
     }
